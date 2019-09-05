6ae65a59a8f81e6989619a4c5941748db96ec636
JO <jolyon129@outlook.com>
2019-09-04 21:52:45 -0400

    On a spree

--- src/main/java/jolyon/AmazonLongestStringWithout3Consecutive.java
+++
@@ -1,5 +0,0 @@
-package com.leetcode.jolyon;
-
-public class AmazonLongestStringWithout3Consecutive {
-
-}
--- src/main/java/jolyon/LC418SentenceScreenFitting.java
+++
@@ -1,5 +0,0 @@
-package com.leetcode.jolyon;
-
-public class LC418SentenceScreenFitting {
-
-}
